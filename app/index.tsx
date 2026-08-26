9cd3f038cbaef8ca9f693cd7ddf19f6d88c599b2
Benicio Khalel <beniciokhalel@gmail.com>
2026-08-26 20:41:14 -0300

    Feat:Implementando as imagens de categoria

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,34 @@
-import {FlatList, Pressable, Text, View} from "react-native";
+import {FlatList, Pressable, Text, View, Image} from "react-native";
 import {offers} from "@/constants"
 import {SafeAreaView} from "react-native-safe-area-context";
 import './global.css'
+import { CATEGORIES } from "@/constants";
+import pressable from "react-native-gesture-handler/src/components/Pressable";
+import {Fragment} from "react";
+
 
 
 export default function Index() {
     return (
         <SafeAreaView>
             <FlatList
-                data={offers}
-                renderItem={({item, index}) => {
+                data = {offers}
+                renderItem = {({item, index}) => {
                     return (
-                        <View>
-                            <Pressable className="offer-card">
-                                <Text>{item.title}</Text>
-                            </Pressable>
-                        </View>
+                        <Pressable className = "offer-card" style = {{ backgroundColor : item.color }}>
+                            {({ pressed }) => (
+                                <Fragment>
+                                    <View className = {"h-full w-1/2"}>
+                                        <Text>{index}</Text>
+                                        <Text>{item.title}</Text>
+                                        <Image source = { item.image } className = {"size-full"} resizeMode = {"contain"}/>
+                                    </View>
+                                </Fragment>
+                             )}
+                        </Pressable>
                     )
-            }}
+                }
+            }
             />
         </SafeAreaView>
     );
